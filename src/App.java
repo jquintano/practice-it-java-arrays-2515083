e6d45edae05e4270cccb59f36b00962c46f1a820
jquintano <76599022+jquintano@users.noreply.github.com>
2024-04-18 06:27:27 +0000

    reverse array

--- src/App.java
+++ src/App.java
@@ -3,7 +3,11 @@ import java.util.Arrays;
 public class App {
     
   public static void reverse(int[] arr) {
-    
+    for (int i = 0; i < arr.length / 2; i++) {
+      int temp = arr[i];
+      arr[i] = arr[arr.length - 1 - i];
+      arr[arr.length - 1 - i] = temp;
+    }
   }
   
   public static void main(String args[]) {
