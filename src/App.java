5d8ecc31d3f4e67ecb18fa965c2a4665cbc18667
jquintano <76599022+jquintano@users.noreply.github.com>
2024-04-18 04:18:24 +0000

    implement find second smallest

--- src/App.java
+++ src/App.java
@@ -1,32 +1,68 @@
+import java.util.Arrays;
+
 public class App {
   
   public static Integer findSecondSmallestItem(Integer[] arr) {
-    return 0;
+    if (arr.length <= 1) {
+      return null;
+    }
+    Arrays.sort(arr);
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] != arr[i+1]) {
+        return arr[i+1];
+      }
+      return null;
+    }
+    return arr[1];
   }
+  
+  public static Integer findSecondSmallestItem2(Integer[] arr) {
+    int smallest = Integer.MAX_VALUE;
+    int secondSmallest = Integer.MAX_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+      int current = arr[i];
+
+      if (current < smallest) {
+        secondSmallest = smallest;
+        smallest = current;
+      } else if (current < secondSmallest && current != smallest) {
+        secondSmallest = current;
+      }
+    }
+    // System.out.println("SMALLEST: "+ smallest);
+    // System.out.println("SECOND SMALLEST: " + secondSmallest);
+    
+    if (secondSmallest == Integer.MAX_VALUE){
+      return null;
+    }
+    
+    return secondSmallest;
+  }
+    
     
   public static void main(String args[]) {
     Integer[] arr = new Integer[]{ 5, 8, 3, 2, 6 };
-    System.out.println(findSecondSmallestItem(arr));
+    System.out.println(findSecondSmallestItem2(arr));
     
     Integer[] arr2 = new Integer[]{ 3, 8, 5, 2, 6 };
-    System.out.println(findSecondSmallestItem(arr2));
+    System.out.println(findSecondSmallestItem2(arr2));
     
     Integer[] arr3 = new Integer[]{ 6, 8, 5, 2, 3 };
-    System.out.println(findSecondSmallestItem(arr3));
+    System.out.println(findSecondSmallestItem2(arr3));
     
     Integer[] arr4 = new Integer[]{ 3, 3, 3, 3, 3 };
-    System.out.println(findSecondSmallestItem(arr4));
+    System.out.println(findSecondSmallestItem2(arr4));
     
     Integer[] arr5 = new Integer[]{ 3, 3, 3, 2, 3 };
-    System.out.println(findSecondSmallestItem(arr5));
+    System.out.println(findSecondSmallestItem2(arr5));
     
     Integer[] arr6 = new Integer[]{ 3, 4, 3, 3, 3 };
-    System.out.println(findSecondSmallestItem(arr6));
+    System.out.println(findSecondSmallestItem2(arr6));
     
     Integer[] arrEmpty = new Integer[] {};
-    System.out.println(findSecondSmallestItem(arrEmpty));
+    System.out.println(findSecondSmallestItem2(arrEmpty));
     
     Integer[] arrOne = new Integer[] { 1 };
-    System.out.println(findSecondSmallestItem(arrOne));
+    System.out.println(findSecondSmallestItem2(arrOne));
     }
 }
