75c26ed764a171c86189ce3e996edc947e14304d
jquintano <76599022+jquintano@users.noreply.github.com>
2024-04-18 06:05:20 +0000

    implement rotate right function

--- src/App.java
+++ src/App.java
@@ -1,14 +1,18 @@
 import java.util.Arrays;
 
 public class App {
-  
+
   public static Object[] rotateRight(Object[] arr) {
-	  return arr;
+    Object[] result = new Object[arr.length];
+    for (int i = 0; i < result.length; i++) {
+      result[(i + 1) % result.length] = arr[i];
+    }
+    return result;
   }
-  
+
   public static void main(String args[]) {
-    Object[] arr = new Integer[] {1, 2, 3, 4, 5};
-      
+    Object[] arr = new Integer[] { 1, 2, 3, 4, 5 };
+
     arr = rotateRight(arr);
     System.out.println(Arrays.toString(arr));
     arr = rotateRight(arr);
@@ -19,14 +23,19 @@ public class App {
     System.out.println(Arrays.toString(arr));
     arr = rotateRight(arr);
     System.out.println(Arrays.toString(arr));
-    
-    Object[] arr2 = new String[] {"Hello", "there", "my friend"};
-    
+
+    Object[] arr2 = new String[] { "Hello", "there", "my friend" };
+
     arr2 = rotateRight(arr2);
     System.out.println(Arrays.toString(arr2));
     arr2 = rotateRight(arr2);
     System.out.println(Arrays.toString(arr2));
     arr2 = rotateRight(arr2);
     System.out.println(Arrays.toString(arr2));
+  }
+
+  @Override
+  public String toString() {
+    return "App []";
   }
 }
