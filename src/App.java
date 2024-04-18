64c8eea0ad11f73f5e4c73ad37f3d7c7c5ac7d80
jquintano <76599022+jquintano@users.noreply.github.com>
2024-04-18 02:50:31 +0000

    working with array data

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
   
   public static void main(String[] args) {
-    
+    double[] lotteryNums = {1,2,3,4,5}; //new double[5];
+    System.out.println(lotteryNums[2]);
+    lotteryNums[2] = 40;
+    System.out.println(lotteryNums[2]);
   }
 }
